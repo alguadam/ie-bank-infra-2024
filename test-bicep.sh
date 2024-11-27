2e6127a51bd31973a532ebe05000e7c7787de5df
Anna <apayne.ieu2022@student.ie.edu>
2024-11-27 03:20:31 +0100

    fixed test-bicep.sh

--- test-bicep.sh
+++ test-bicep.sh
@@ -3,10 +3,12 @@
 RESOURCE_GROUP=BCSAI2024-DEVOPS-STUDENTS-B-DEV
 LOCATION="East US"
 
-az login
-az account set --subscription e0b9cada-61bc-4b5a-bd7a-52c606726b3b
+# az login
+# az account set --subscription e0b9cada-61bc-4b5a-bd7a-52c606726b3b
 
-az group create --name $RESOURCE_GROUP --location $LOCATION
+if ! az group show --name $RESOURCE_GROUP > /dev/null 2>&1; then
+  az group create --name $RESOURCE_GROUP --location $LOCATION
+fi
 
 echo "Validating Bicep file: ./main.bicep"
 az bicep build --file ./main.bicep
